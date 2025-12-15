b806113ef3dbc52d69953ab24dbce22f971f2042
vmilesc <vmilesc@upv.edu.es>
2025-12-15 17:31:07 +0100

    actualizada HashTable

--- HashTable.h
+++ HashTable.h
@@ -1,62 +1,88 @@
 #ifndef HASHTABLE_H
-#define HASTABLE_H
+#define HASHTABLE_H
 
 #include <ostream>
 #include <stdexcept>
 #include "Dict.h"
 #include "TableEntry.h"
 
-#include "../../Practica1/PRA_2324_P1/ListLinked.h"
+#include "../../Practica1/PRA_2324_P1/ListLinked.h"  // ¡¡¡¡MODIFICAR!!!!
 
 template <typename V>
 class HashTable: public Dict<V> {
-	private:
-		int n;
+
+    private:
+        	int n;
 		int max;
 		ListLinked<TableEntry<V>>* table;
 
-		int h(std::string key){
-			int temp = 0;
-			for(char c : key){
-				temp += int(c);
-			}
-			return temp % max;
-		}
-	public:
-		HashTable(int size){
-			max = size;
-			n = 0;
-			table = new ListLinked<TableEntry<V>>[max];
-		}
-
-		~HashTable(){
-			delete[] table;;
-		}
-
-		int capacity(){
-			return max;
-		}
-
-		friend std::ostream&operator<<(std::ostream &out, const HashTable<V> &th){
-			for(int i = 0; i < th.max; i++) {
-				out << th.table[i] << std::endl;
-			}
-			return out;
-		}
-
-		V operator[](std::string key) {
-			int index = h(key);
-    			ListLinked<TableEntry<V>>& list = table[index];
+    public:
+        int h(std::string key){
+	
+		int i=0;
+		char c = key.at(i);
+		int valor=0;
+	
+		while(i < key.length()){
     
-    			for (auto& entry : list) {
-        			if (entry.key == key) {
-            				return entry.value;
-        			}
-    			}
-    			throw std::runtime_error("Clave no encontrada");
+    		valor = valor + int(key.at(i));
+    		i++;
+		}
+	
+		return (valor % max);
+	}	
+
+	HashTable(int size){
+
+		max = size;
+		n = 0;
+		table = new TableEntry<V>* [max];
+		for(int i=0;i<max;i++){
+			table[i]=nullptr;
+		}
+	};
+
+
+
+	~HashTable(){
+	
+		for(int i=0;i<max;i++){
+			if(table[i] != nullptr){
+
+                        delete table[i];
+                	
+			}
+		}
+		delete[] table;
+
+	}
+
+	int capacity(){
+
+		return max;
+
+	}
+        friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th){
+
+		for(int i=0;i<th.max;i++){
+
+
+		out<<th.table[i]<<"\n";
+			
 		}
 
+		return out;
+	}
+	V operator[](std::string key){
 
+	if(key!= ""){
+		return key;
+	}else {
+		throw std::runtime_error("La clave no puede estar vacía.");
+	}
+
+
+	}
 };
 
 #endif
